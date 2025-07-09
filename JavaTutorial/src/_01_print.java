3dd46c3682c5d004c24e86a2310d0f19ced22e99
Pranjal Ashok Singh <pranjalsingh.ak@gmail.com>
2025-07-10 00:51:18 +0530

    _01_print

---
+++ JavaTutorial/src/_01_print.java
@@ -0,0 +1,6 @@
+public class _01_print {
+//    just type "main" shortcut for the below function
+    public static void main(String[] args) {
+        System.out.println("Pranjal Starting Java");
+    }
+}
